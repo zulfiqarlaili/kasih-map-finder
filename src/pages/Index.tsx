dc5791bbaf65a6eff965978400c1542c20c8c798
Zulfiqar <zulfiqar.laili@setel.com>
2025-08-26 20:27:12 +0800

    Fix center me button overlap

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -159,7 +159,7 @@ const Index = () => {
             variant="default"
             size="sm"
             onClick={() => setIsMobileSheetOpen(true)}
-            className="md:hidden fixed bottom-4 right-4 z-30 rounded-full shadow-elegant"
+            className="md:hidden fixed bottom-4 right-16 z-30 rounded-full shadow-elegant"
           >
             <List className="w-5 h-5 mr-2" /> Stores
           </Button>
